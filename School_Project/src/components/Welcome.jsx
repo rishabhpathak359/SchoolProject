af4b96a7e3280db4da79bf0c2c42f8c45492f821
Rishabh <rishabh09p@gmail.com>
2024-06-29 15:57:51 +0530

    ..

--- School_Project/src/components/Welcome.jsx
+++ School_Project/src/components/Welcome.jsx
@@ -6,10 +6,10 @@ const Welcome = () => {
   return (
     <div className="flex flex-col items-center my-8">
       {/* Main Heading */}
-      <h1 className="text-4xl md:text-5xl  font-extrabold mb-10 text-center">Welcome</h1>
+      <h1 className="text-4xl md:text-5xl font-extrabold mb-10 text-center">Welcome</h1>
 
       {/* Container for Director Section and Quote */}
-      <div className="flex md:flex-row flex-col justify-start md:justify-around items-center  w-full bg-gray-100 p-8 rounded-lg shadow-md">
+      <div className="flex md:flex-row flex-col justify-start md:justify-around items-center w-full bg-gray-100 p-8 rounded-lg shadow-md">
         {/* Director Section */}
         <div className="flex-shrink-0 mb-6 md:mb-0">
           <img
@@ -36,11 +36,21 @@ const Welcome = () => {
       </div>
 
       {/* Welcome Message */}
-      <div className="md:w-3/4 w-full mt-8">
+      <div className="md:w-3/4 w-full mt-8 text-center">
+        <p className="text-lg text-gray-700 mb-4">
+          Dear School Community,
+        </p>
+        <p className="text-lg text-gray-700 mb-4">
+          As we embark on a new academic year, I am thrilled to welcome both returning and new students. Our goal remains to foster a supportive and dynamic learning environment. Together, we can achieve excellence and make this year truly memorable.
+        </p>
+        <p className="text-lg text-gray-700 mb-4">
+          Best wishes,
+        </p>
+        <p className="text-lg text-gray-700 font-semibold">
+          Dr. Rakesh Sharma
+        </p>
         <p className="text-lg text-gray-700">
-          Welcome to our school! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nulla convallis libero ut lacus placerat, ac maximus eros tempor.
-          Integer nec odio ut urna condimentum ullamcorper. In vitae elit sed tortor condimentum eleifend.
+          School Director
         </p>
       </div>
     </div>
